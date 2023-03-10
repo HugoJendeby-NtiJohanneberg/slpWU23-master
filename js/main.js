5a156775c47f4f49a0d1df31584c6389fa0ab52e
HugoJendeby-NtiJohanneberg <119052994+HugoJendeby-NtiJohanneberg@users.noreply.github.com>
2023-03-10 14:35:09 +0100

    V. 10

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let button = document.querySelector("#arrowleft").addEventListener("click", showmenu);
+
+
+function showmenu(){
+    console.log("huh")
+    let bev = document.querySelector("#beverage1");
+    bev.classList.toggle("show");
+
+    let bev2 = document.querySelector("#beverage2");
+    bev2.classList.toggle("show2");
+}
