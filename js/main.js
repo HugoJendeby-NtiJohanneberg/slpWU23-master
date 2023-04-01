ce6f2aa96b8caf3490d8d03a009b0ba09bae8fa6
HugoJendeby-NtiJohanneberg <hugo.jendeby@elev.ga.ntig.se>
2023-04-01 21:59:37 +0200

    dag1_hemma

--- js/main.js
+++ js/main.js
@@ -1,12 +1,49 @@
 let button = document.querySelector(".selector").addEventListener("click", showmenu);
-
+let i = 0
 
 function showmenu(){
     console.log("huh")
     let bev = document.querySelector("#beverage1");
-    bev.classList.toggle("show");
-
     let bev2 = document.querySelector("#beverage2");
-    bev2.classList.toggle("show2");
+    let bev3 = document.querySelector("#beverage3");
 
+    if (i == 0) {
+
+        bev3.classList.remove("hide");
+        bev2.classList.remove("hide2");
+    
+        bev3.classList.add("show");
+        bev2.classList.add("show2");
+    
+        setTimeout(function() {
+            bev3.classList.remove("show");
+            bev2.classList.remove("show2");
+    
+            bev3.classList.add("hide");
+            bev2.classList.add("hide2");
+        }, 1000);
+        
+        i++;
+    }
+    else if (i == 1){
+        
+       
+        bev3.classList.remove("hide");
+        
+        bev.classList.add("show");
+        bev3.classList.add("show2");
+    
+        setTimeout(function() {
+            bev.classList.remove("show");
+            bev3.classList.remove("show2");
+            bev.classList.add("hide");
+            bev3.classList.add("hide2");
+        }, 1000);
+
+        i++;
+    }
+
+    
+    console.log(i)
 }
+
